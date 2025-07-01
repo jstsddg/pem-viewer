44d3a3a81fca21de96da67890d2df4897394ddc4
Joost Ole Seddig <jst_sddg@outlook.com>
2025-07-01 15:15:42 +0200

    Remove unused and unmaintained wee_alloc

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,6 @@ use wasm_bindgen::prelude::*;
 use web_sys::{Document, Element};
 use x509_parser::{nom::AsBytes, prelude::*};
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
